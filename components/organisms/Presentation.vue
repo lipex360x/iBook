b74a11138b5eb9034f38f756ab13754b7b914b40
Felipe Borges <lipex360@gmail.com>
2021-11-27 16:32:36 -0300

    about page added

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="organisms-presentation">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ab sint ex
+      sed blanditiis quam velit? Laborum harum vel iste. Voluptas deleniti
+      suscipit ducimus quo sit ipsa autem ipsum eaque!
+    </p>
+    <img src="@/assets/img/presentation.svg" alt="iBook Presentation" />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  // vue here
+})
+</script>
+
+<style lang="scss" scoped>
+.organisms-presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
---
+++ components/templates/AboutTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="templates-about">
+    <PageTitle text="O que é o " strong="iBook?" />
+    <Presentation />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.templates-about {
+  display: grid;
+  grid-gap: 4rem;
+}
+</style>
