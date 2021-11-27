e764cc9dfcb837387a3d6a9709b2e60458c7afb2
Felipe Borges <lipex360@gmail.com>
2021-11-27 18:42:02 -0300

    vuex updated

--- store/books.ts
+++ store/books.ts
@@ -2,7 +2,7 @@ import { Module, VuexModule } from 'vuex-module-decorators'
 
 @Module({ name: 'books', stateFactory: true, namespaced: true })
 export default class Sample extends VuexModule {
-  books = ['livro1', 'livro2', 'livro3']
+  private books = ['livro1', 'livro2', 'livro3']
 
   // @Action
   // public async index() {}
