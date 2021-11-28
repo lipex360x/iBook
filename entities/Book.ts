e4939ec42d16164692dcb56e6a396a8fc0c1174c
Felipe Borges <lipex360@gmail.com>
2021-11-28 12:52:34 -0300

    entity Book added

--- entities/Book.ts
+++ entities/Book.ts
@@ -1,3 +1,21 @@
+interface CategoryProps {
+  id: number
+  name: string
+}
+
+interface PageProps {
+  id: number
+  text: string
+  pageNumber: number
+}
+
 export interface BookProps {
   id: number
+  author: string
+  releaseDate: string
+  title: string
+  description: string
+  cover: string
+  categories: CategoryProps[]
+  pages: PageProps[]
 }
