5d5854982c0795ed303ee8cf10ccb5c85719cc99
Felipe Borges <lipex360@gmail.com>
2021-11-28 00:09:20 -0300

    entity Book added

---
+++ entities/Book.ts
@@ -0,0 +1,3 @@
+export interface BookProps {
+  id: number
+}
--- store/books.ts
+++ store/books.ts
@@ -1,21 +1,13 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 
+import { BookProps } from '@/entities'
+
 @Module({ name: 'books', stateFactory: true, namespaced: true })
 export default class Books extends VuexModule {
-  private count = 0
+  private books = [] as BookProps[]
 
   public get $count() {
-    return this.count
-  }
-
-  @Mutation
-  private INCREMENT(number: number) {
-    this.count += number
-  }
-
-  @Action
-  public increment() {
-    this.context.commit('INCREMENT', 1)
+    return this.books
   }
 
   // @Action
